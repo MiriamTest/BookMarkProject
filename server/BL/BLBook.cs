c69560f4ea32bed92d6d78bfb6704d9b17b08688
GRTSulotionsSTD <tov9295@gmail.com>
2018-10-31 21:20:55 +0200

    blbooks

--- server/BL/BLBook.cs
+++ server/BL/BLBook.cs
@@ -11,41 +11,41 @@ using Entities;
 namespace BL
 {
   public static class BLBook
+  {
+    public static Books[] allBooks()
     {
-        public static Books[] allBooks()
-        {
-            return DalBook.allBooks();
-        }
-        public static Categories[] allCategories()
-        {
-            return DalBook.allCategories();
-        }
-        public static Books getBook(BooksInLibrary[] id)
-        {
-            return DalBook.getBook(id);
-        }
-        public static Books addNewBook(Books newBook)
-        {
-            return DalBook.addNewBook(newBook);
-        }
+      return DalBook.allBooks();
+    }
+    public static Categories[] allCategories()
+    {
+      return DalBook.allCategories();
+    }
+    public static Books getBook(BooksInLibrary[] id)
+    {
+      return DalBook.getBook(id);
+    }
+    public static Books addNewBook(Books newBook)
+    {
+      return DalBook.addNewBook(newBook);
+    }
     public static bool addBook(BooksInLibrary newBook)
     {
       newBook.IdStatus = 2;
       return DalBook.addBook(newBook);
     }
-    public static BooksInLibrary[] searchBook(Dal.Model.SearchObj searchObj )
-        {
+    public static BooksInLibrary[] searchBook(Dal.Model.SearchObj searchObj)
+    {
 
-            return DalBook.searchBook(searchObj);
-
-
+      return DalBook.searchBook(searchObj);
 
 
-        }
-        public static StatusLending getStatus(int id)
-        {
-            return DalBook.getStatus(id);
-        }
+
+
+    }
+    public static StatusLending getStatus(int id)
+    {
+      return DalBook.getStatus(id);
+    }
     public static BooksInLibrary getBook(int bookId)
     {
       return DalBook.getBook(bookId);
@@ -54,7 +54,22 @@ namespace BL
     {
       return DalBook.getBookObj(bookId);
     }
-
-   
+    public static bool deleteBook(int bookId)
+    {
+      return DalBook.deleteBook(bookId);
+    }
+    public static Object getSearchObj()
+    {
+      return DalBook.getSearchObj();
+    }
+    public static StatusLending[] allStatuses()
+    {
+      return DalBook.allStatuses();
+        
+    }
+    public static bool editBook(BooksInLibrary book)
+    {
+      return DalBook.editBook(book);
+    }
   }
 }
